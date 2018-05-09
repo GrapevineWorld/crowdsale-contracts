9bad7e623c798bd16a2b9c9e7f6edf07248d45de
abmiladi <35726620+abmiladi@users.noreply.github.com>
2018-05-09 11:20:16 +0200

    testnet/ganache accounts configurations aligned.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,37 +3,24 @@ const GrapevineToken = artifacts.require('./GrapevineToken.sol');
 
 module.exports = function (deployer, network, accounts) {
   var openingTime;
-
+  // owner of the crowdsale
+  const  owner = web3.eth.accounts[0];
+  
+  // wallet where the ehter will get deposited
+  const  wallet = web3.eth.accounts[1];
+  
+  // tokenWallet where the crowdsale tokens are located!
+  const  tokenWallet = web3.eth.accounts[2];
+  
   const rate = new web3.BigNumber(1);
   const cap = 1000 * (10 ** 18);
   var grapevineTokenInstance;
   var grapevineCrowdsaleInstance;
   let totalSupply;
-  var owner;
-  var wallet;
-  var tokenWallet;
   if (network === 'rinkeby') {
     openingTime = web3.eth.getBlock('latest').timestamp + 300; // five minutes in the future
-
-    // owner of the crowdsale
-    owner = '0x3600455a40C45C18f50992AE8AED82e1a92D6c12';
-
-    // wallet where the ehter will get deposited
-    wallet = '0xae927979f47f3c8731db7495452be5C47273B410';
-
-    // tokenWallet where the crowdsale tokens are located!
-    tokenWallet = '0x17dE97283B149A1a1a8B862853bF2F18BdAdff8D';
   } else {
     openingTime = web3.eth.getBlock('latest').timestamp + 60; // one minute in the future
-
-    // owner of the crowdsale
-    owner = web3.eth.accounts[0];
-
-    // wallet where the ehter will get deposited
-    wallet = web3.eth.accounts[1];
-
-    // tokenWallet where the crowdsale tokens are located!
-    tokenWallet = web3.eth.accounts[2];
   }
 
   // const closingTime = openingTime + 86400 * 20; // 20 days
