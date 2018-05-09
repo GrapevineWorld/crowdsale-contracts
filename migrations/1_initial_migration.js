7e30c9faf074d6d92ad3acc494b8983df67e6921
abmiladi <35726620+abmiladi@users.noreply.github.com>
2018-05-09 16:12:50 +0200

    missing import added

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,6 +1,7 @@
+var Migrations = artifacts.require('./Migrations.sol');
+
 module.exports = function (deployer, network, accounts) {
   // owner of the contract
   var owner = web3.eth.accounts[0];
-  
   deployer.deploy(Migrations, { from: owner });
 };
