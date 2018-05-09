cc4b15a59db0aaf44900e4180508dd485c70b829
abmiladi <35726620+abmiladi@users.noreply.github.com>
2018-05-09 15:18:56 +0200

    set an owner of the contract.

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,6 @@
-var Migrations = artifacts.require('./Migrations.sol');
-
-module.exports = function (deployer) {
-  deployer.deploy(Migrations);
+module.exports = function (deployer, network, accounts) {
+  // owner of the contract
+  var owner = web3.eth.accounts[0];
+  
+  deployer.deploy(Migrations, { from: owner });
 };
