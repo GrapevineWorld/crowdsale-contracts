f40606227a3d6be619679cff92838814138e1eea
abmiladi <35726620+abmiladi@users.noreply.github.com>
2018-05-09 15:18:02 +0200

    removed default address from rinkeby network
    all the contracts are deployed with a from attribute to avoid having a default account.

--- truffle.js
+++ truffle.js
@@ -16,8 +16,7 @@ module.exports = {
     rinkeby: {
       host: "localhost", // Connect to geth on the specified
       port: 8545,
-      from: "0x3600455a40C45C18f50992AE8AED82e1a92D6c12", // default address to use for any transaction Truffle makes during migrations
-      network_id: 4,
+      network_id: "*",
       gas: 4612388 // Gas limit used for deploys
     }
   }
